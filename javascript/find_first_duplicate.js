428e62474362d8b94ee485416fafd33f26f86219
pedro <vincentypedro@gmail.com>
2024-09-17 09:36:17 -0400

    solution

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,30 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  let hash = []
+  for(let i = 0; i<arr.length; i++){
+    // console.log(arr[i])
+    if(hash.includes(arr[i])){
+      return arr[i]
+    }
+    hash.push(arr[i])
+    // console.log(hash)
+  }
+  return -1
+
 }
+// function findFirstDuplicate(arr){
+//   let duplicate = new Set()
+//   for(let i = 0; i<arr.length; i++){
+//     if(duplicate.has(arr[i])){
+//       return arr[i]
+//     } else{
+//       duplicate.add(arr[i])
+//     }
+//   }
+//   return -1
+// }
+
+
 
 if (require.main === module) {
   // add your own tests in here
